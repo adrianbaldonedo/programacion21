7ac4cdb71539b243b03782ea91292ce8493e689d
Adrian BM <adrianabril1989@gmail.com>
2021-03-20 23:08:39 +0100

    main cambiado

---
+++ src/prueba/Sumador.java
@@ -0,0 +1,10 @@
+package prueba;
+/**
+ *
+ * @author adrian
+ */
+public class Sumador {
+    public static float sumar(float a,float b){
+        return a+b;
+    }
+}
---
+++ src/prueba/programa.java
@@ -0,0 +1,12 @@
+package prueba;
+
+/**
+ *
+ * @author adrian
+ */
+public class programa {
+    public static void main(String[] args) {
+        System.out.println(Sumador.sumar(15f,4f));
+    }
+    
+}
